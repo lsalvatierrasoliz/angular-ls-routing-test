556f542f1256f4ea54334b1371b9dcdf4242c210
lsalvatierrasoliz <adolfo_luis_9@hotmail.com>
2021-07-05 17:45:40 -0400

    in memory db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppData } from './app-data';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    InMemoryWebApiModule.forRoot(AppData, { delay: 1000 }),
+    AppRoutingModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
